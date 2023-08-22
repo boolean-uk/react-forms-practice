be73a5b0f9a9396ef90edbcc0069e1c9a70d28d8
iliask796 <iliaskal7@gmail.com>
2023-08-22 11:44:26 +0300

    All Finished

--- src/App.js
+++ src/App.js
@@ -3,29 +3,41 @@ import "./App.css";
 
 export default function App() {
   
-  //TODO: Add your state fields here
+  const [formData, setFormData] = useState({})
+
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(formData)
+  }
 
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input type="text" name="name" required onChange={handleChange} />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input type="text" name="address" onChange={handleChange} />
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input type="tel" name="phone" onChange={handleChange} />
           </label>
-
           <label>
             Email
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={handleChange} />
           </label>
         </div>
 
@@ -36,10 +48,11 @@ export default function App() {
               name="complaint"
               rows="10"
               placeholder="You can complain here"
+              onChange={handleChange}
             ></textarea>
           </label>
 
-          <div className="form__radio-group">
+          <div className="form__radio-group" onChange={handleChange}>
             <p>How do you want to be contacted? </p>
             <label>
               <input type="radio" name="contact" value="phone" />
@@ -64,10 +77,10 @@ export default function App() {
 
           <label>
             I agree you take my data, and do whatever
-            <input type="checkbox" name="consent" id="consent" />
+            <input type="checkbox" name="consent" id="consent" onChange={(event) => handleChange({target:{name: event.target.name, value: event.target.checked}})}/>
           </label>
         </div>
-        <input type="submit" value="Submit!" />
+        <input type="submit" value="Submit!"/>
       </form>
     </>
   );
