3609257371cc432651f74d816962144f4a3c0cbc
Faiza <faraz_ul_islam@yahoo.com>
2023-11-01 19:28:14 +0000

    react pratice form

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,80 @@
 import { useState } from "react";
 import "./App.css";
 
+
+
+const Initial_Complaining_Form = {
+  name: "",
+  address: "",
+  phone: "",
+  email: "",
+  complaint: "",
+  contact: "",
+  consent: false
+}
+
+
 export default function App() {
 
   //TODO: Add your state fields here
+  const [complainingForm, setComplainingForm] = useState(Initial_Complaining_Form)
+
+
+  const complainingFormSubmit = (event) => {
+    event.preventDefault()
+    //console.log(form)
+
+   const options = {
+      method :'POST',
+      Headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(complainingForm)
+
+    }
+    fetch('http://localhost:3000', options)
+
+    setComplainingForm(Initial_Complaining_Form)
+  }
+
+
+  function complainingFormHandler(event) {
+    const { checked, value, name, type } = event.target
+
+    if (type === 'checkbox') {
+      setComplainingForm({ ...complainingForm, [name]: checked })
+    } else {
+      setComplainingForm({...complainingForm, [name] : value})
+    }
+  }
+
+  //const updatedValue = {
+  // [name]: type === "checkbox" ? checked : value,
+  //;
+
+  const contactOptions = ["phone", "email", "letter"];
+
+
 
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={complainingFormSubmit}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input type="text" name="name" required value={complainingForm.name} onChange={complainingFormHandler} />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input type="text" name="address" value={complainingForm.address} onChange={complainingFormHandler} />
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input type="tel" name="phone" value={complainingForm.phone} onChange={complainingFormHandler} />
           </label>
 
           <label>
             Email
-            <input type="email" name="email" />
+            <input type="email" name="email" value={complainingForm.email} onChange={complainingFormHandler} />
           </label>
         </div>
 
@@ -36,35 +85,37 @@ export default function App() {
               name="complaint"
               rows="10"
               placeholder="You can complain here"
+              value={complainingForm.complaint}
+              onChange={complainingFormHandler}
             ></textarea>
           </label>
 
           <div className="form__radio-group">
             <p>How do you want to be contacted? </p>
             <label>
-              <input type="radio" name="contact" value="phone" />
+              <input type="radio" name="contact" value="phone" checked={complainingForm.contact === 'phone'} onChange={complainingFormHandler} />
               Phone
             </label>
 
             <label>
-              <input type="radio" name="contact" value="email" />
+              <input type="radio" name="contact" value="email" checked={complainingForm.contact === 'email'} onChange={complainingFormHandler} />
               Email
             </label>
 
             <label>
-              <input type="radio" name="contact" value="post" />
+              <input type="radio" name="contact" value="post" checked={complainingForm.contact === 'post'} onChange={complainingFormHandler} />
               Slow Mail
             </label>
 
             <label>
-              <input type="radio" name="contact" value="none" />
+              <input type="radio" name="contact" value="none" checked={complainingForm.contact === 'none'} onChange={complainingFormHandler} />
               No contact!
             </label>
           </div>
 
           <label>
             I agree you take my data, and do whatever
-            <input type="checkbox" name="consent" id="consent" />
+            <input type="checkbox" name="consent" id="consent" checked={complainingForm.consent} onChange={complainingFormHandler} />
           </label>
         </div>
         <input type="submit" value="Submit!" />
