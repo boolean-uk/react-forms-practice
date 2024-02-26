c5a8ee80ac038c10079a9ed06968981ae430e215
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-26 13:34:51 +0100

    Completed Extension

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,27 @@ import "./App.css";
 export default function App() {
 
   //TODO: Add your state fields here
-  const [name, setName] = useState('')
-  const [address, setAddress] = useState('')
-  const [phone, setPhone] = useState('')
-  const [email, setEmail] = useState('')
-  const [text, setText] = useState('')
-  const [contactMethod, setContactMethod] = useState(null)
-  const [agree, setAgree] = useState(false)
+  const [form, setForm] = useState({
+    name: '',
+    address: '',
+    phone: '',
+    email: '',
+    text: '',
+    contactMethod: null,
+    agree: false
+  });
 
   const logResults = () => {
-    console.log(`Name:${name}, Address: ${address}, Phone: ${phone}, Email: ${email}, Text: ${text}, Contact method: ${contactMethod}, Agrees to stuff: ${agree}`)
+    console.log(`
+    Name:${form.name}, 
+    Address: ${form.address}, 
+    Phone: ${form.phone}, 
+    Email: ${form.email}, 
+    Text: ${form.text}, 
+    Contact method: ${form.contactMethod}, 
+    Agrees to stuff: ${form.agree}`)
   }
-  
+
   return (
     <>
       <form className="form">
@@ -26,8 +35,8 @@ export default function App() {
             <input
               type="text"
               name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={form.name}
+              onChange={(e) => setForm({ ...form, name: e.target.value })}
               required
             />
           </label>
@@ -36,8 +45,8 @@ export default function App() {
             <input
               type="text"
               name="address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
+              value={form.address}
+              onChange={(e) => setForm({ ...form, address: e.target.value })}
               required
             />
           </label>
@@ -46,8 +55,8 @@ export default function App() {
             <input
               type="tel"
               name="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              value={form.phone}
+              onChange={(e) => setForm({ ...form, phone: e.target.value })}
             />
           </label>
           <label>
@@ -55,8 +64,8 @@ export default function App() {
             <input
               type="email"
               name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
             />
           </label>
         </div>
@@ -68,39 +77,73 @@ export default function App() {
               name="complaint"
               rows="10"
               placeholder="You can complain here"
-              value={text}
-              onChange={e => setText(e.target.value)}
+              value={form.text}
+              onChange={(e) => setForm({ ...form, text: e.target.value })}
             ></textarea>
           </label>
           <div className="form__radio-group">
             <p>How do you want to be contacted? </p>
             <label>
-              <input type="radio" name="contact" value="phone" onChange={e => setContactMethod(e.target.value)}/>
+              <input
+                type="radio"
+                name="contact"
+                value="phone"
+                onChange={(e) =>
+                  setForm({ ...form, contactMethod: e.target.value })
+                }
+              />
               Phone
             </label>
 
             <label>
-              <input type="radio" name="contact" value="email" onChange={e => setContactMethod(e.target.value)}/>
+              <input
+                type="radio"
+                name="contact"
+                value="email"
+                onChange={(e) =>
+                  setForm({ ...form, contactMethod: e.target.value })
+                }
+              />
               Email
             </label>
 
             <label>
-              <input type="radio" name="contact" value="post" onChange={e => setContactMethod(e.target.value)}/>
+              <input
+                type="radio"
+                name="contact"
+                value="post"
+                onChange={(e) =>
+                  setForm({ ...form, contactMethod: e.target.value })
+                }
+              />
               Slow Mail
             </label>
 
             <label>
-              <input type="radio" name="contact" value="none" onChange={e => setContactMethod(e.target.value)}/>
+              <input
+                type="radio"
+                name="contact"
+                value="none"
+                onChange={(e) =>
+                  setForm({ ...form, contactMethod: e.target.value })
+                }
+              />
               No contact!
             </label>
           </div>
 
           <label>
             I agree you take my data, and do whatever
-            <input type="checkbox" name="consent" id="consent" onChange={e => setAgree(e.target.value)}/>
+            <input
+              type="checkbox"
+              name="consent"
+              id="consent"
+              checked={form.agree}
+              onChange={(e) => setForm({ ...form, agree: e.target.checked })}
+            />
           </label>
         </div>
-        <input type="submit" value="Submit!" onClick={logResults}/>
+        <input type="submit" value="Submit!" onClick={logResults} />
       </form>
     </>
   );
