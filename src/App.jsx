f49212a50d0eb1d845fa61d1e59604c1726d1f61
Angus Townsley <angustownsley@gmail.com>
2024-04-30 11:18:05 +0100

    Extract input components

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ import Input from "./Input"
 export default function App() {
 
   //TODO: Add your state fields here
-  const [formData, setFormData] = useState([])
   return (
     <>
-      <Input formData={formData} setFormData={setFormData} />
+      <Input/>
     </>
   );
 }
